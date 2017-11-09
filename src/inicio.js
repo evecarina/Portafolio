6f6b882f6ef32d9aecfaff1dffef1deb21f9921f
carina120794@gmail.com <you@example.com>
2017-11-09 08:35:19 -0500

    stylos a la seccion contacto

--- src/inicio.js
+++ src/inicio.js
@@ -4,40 +4,54 @@ import { NavLink } from 'react-router-dom';
 import { connect } from "redux-zero/react";
 
 const BarraNav = () => {   
-  return(  
-     
+  return(       
       <nav class="navbar navbar-default">
         <div class="container-fluid">        
           <ul class="nav navbar-nav">         
-            <li className="col-md-4 col-xs-4"><a href="#">Page 1</a></li>
-            <li className="col-md-4 col-xs-4"><a href="#">Page 2</a></li>
-            <li className="col-md-4 col-xs-4"><a href="#">Page 3</a></li>
+            <li className="col-md-4 col-xs-4"><a href="#">Conoceme</a></li>
+            <li className="col-md-4 col-xs-4"><a href="#">Portafolio</a></li>
+            <li className="col-md-4 col-xs-4"><a href="#">Contacto</a></li>
           </ul>
         </div>     
-    </nav>   
-    
- 
-    );   
+    </nav>      
+     );   
 }
 const Portada=()=>{
   return(
-  <div className=" container-fluid portada">
-    <div className="portada-name">
-      <p>EVELYN GUEVARA <br/>
-      FROND-END DEVELOPER
-      </p>
-    </div>
-</div> 
+    <section className="contportada">
+      <div id="start" align="center">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyMyJI3SIy1AqnBS4XUSg-t5DsxgEW2dm1q0aoASOtaDY75RcC" alt=""/>
+        <p>START BOOTSTRAPT</p>
+        <p>Web Developer- Graphic Artist- User Experiencie Designer</p>
+      </div>
+    </section>
   );
-
 }
-const About=()=>{
- 
 
+const Portafolio=()=>{
+  return( 
+  <section className="portafolios">
+  <p align="center">PORTFOLIO</p>
+  <div className="">
+  <div>
+    <div><img src="http://webbingstone.com/wp-content/uploads/services-mobile-marketing-380x380.png"alt=""/></div>
+    <div><img src="http://webbingstone.com/wp-content/uploads/services-mobile-marketing-380x380.png" alt=""/></div>
+    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoZZOwg_YFf8OL3MzY7_Nw707O2ah3c50y_Fxx3FKy32CCUH6QRQ" alt=""/></div>
+  </div>
+  <div>
+    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbh35YAoVjAH_39WGnrbi4lkYpx3AfcpsOKAJ7O5wVlZzb7VRY" alt=""/></div>
+    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbh35YAoVjAH_39WGnrbi4lkYpx3AfcpsOKAJ7O5wVlZzb7VRY" alt=""/></div>
+    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbh35YAoVjAH_39WGnrbi4lkYpx3AfcpsOKAJ7O5wVlZzb7VRY" alt=""/></div>
+  </div>
+  </div>
+
+</section>
+);
 }
+
 const Contact=()=>{
   return(
-  <section class="contacto">
+  <section className="contacto">
     <p align="center">CONTACT ME</p>
 		<p align="center"></p>
 		<div>
@@ -46,21 +60,8 @@ const Contact=()=>{
 			<input  id="numero" type="text" placeholder="Phone Number"/><br/>
 			<input id="mensaje" type="text" placeholder="Message "/><br/>
 			<button id="enviar">Send</button>
-		</div>  
-    <div id="loc2">
-    <div>
-				<p>3481 Melrose Place <br/>
-				Bervely Hills,Ca902010</p>
 		</div>
-    </div>  
-    <div id="loc2_img">
-        <a href="https://www.instagram.com/"><img src="https://github.com/evecarina/freeelancer/blob/master/assets/images/in.png"/></a>
-				<a href="https://twitter.com/"><img src="https://raw.githubusercontent.com/evecarina/freeelancer/master/assets/images/twiter.png"/></a>
-				<a href="gmail.com"><img src="https://raw.githubusercontent.com/evecarina/freeelancer/master/assets/images/gmail.png"/></a>
-				<a href="https://www.facebook.com/"><img src="https://raw.githubusercontent.com/evecarina/freeelancer/master/assets/images/face.png"/></a>
-    </div>
-     
-  </section>
+    </section>
 );
 }
 
@@ -68,7 +69,8 @@ const App=()=>{
   return(
     <div className="barranav">
       <BarraNav/>,
-      <Portada/> 
+      <Portada/>,
+     <Portafolio/>
       <Contact/>    
     </div>    
   );
