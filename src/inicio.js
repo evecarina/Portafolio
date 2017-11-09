5814a9430ea01f18084a2caf7bc313c7dbdb168e
carina120794@gmail.com <you@example.com>
2017-11-09 16:38:57 -0500

    modificacion

--- src/inicio.js
+++ src/inicio.js
@@ -97,7 +97,7 @@ const App=()=>{
     <div className="barranav">
       <BarraNav/>
       <Portada/>
-      <About/>
+      {/* <About/> */}
      <Portafolio/>
       <Contact/>    
     </div>    
